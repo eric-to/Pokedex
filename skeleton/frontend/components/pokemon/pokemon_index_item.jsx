603938fcc29f3aedb2a4399cf6ef45a2338c2a1a
Eric To <erto@berkeley.edu>
2018-12-06 08:21:29 -0800

    Restyle homepage

--- skeleton/frontend/components/pokemon/pokemon_index_item.jsx
+++ skeleton/frontend/components/pokemon/pokemon_index_item.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const PokemonIndexItem = ({ pokemon }) => (
   <div className="pokemon-entry">
-    {/* <img src={pokemon.image_url} alt={pokemon.name}/> */}
-    <span>{pokemon.name}</span>
+    <img src={pokemon.image_url} alt={pokemon.name}/>
+    <div>{pokemon.name}</div>
   </div>
 );
 
